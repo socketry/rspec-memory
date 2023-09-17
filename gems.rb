2f4d3749144175fb332ebfd7e174d49ee20b11af
Marcin Nowicki <pr0d1r2@gmail.com>
2023-09-17 07:03:56 +0200

    Use gem grouping that better reflect reality

--- gems.rb
+++ gems.rb
@@ -15,13 +15,13 @@ group :maintenance, optional: true do
   gem 'bake-modernize', '~> 0.17.8'
 end
 
-group :development do
+group :development, :test do
   gem 'covered', '~> 0.25.0', require: false
+  gem 'guard-rspec', '~> 4.7'
+  gem 'guard-rubocop', '~> 1.5'
 end
 
 group :test do
   gem 'bake-test', '~> 0.2.0'
   gem 'bake-test-external', '~> 0.3.3'
-  gem 'guard-rspec', '~> 4.7'
-  gem 'guard-rubocop', '~> 1.5'
 end
